961f4eeebcf2289b7948c7bcb82c0f6419bf5ee9
kflange <zknsftk1msrk@gmail.com>
2017-12-31 19:27:45 +0900

    refact

--- main.go
+++ main.go
@@ -33,17 +33,29 @@ func main() {
 	}
 	defer fout.Close()
 
+	src_to_out(
+		createReadCSV(finn),
+		createWriteCSV(fout))
+}
+
+func createReadCSV(finn io.Reader) *csv.Reader {
 	reader := csv.NewReader(finn) // utf-8
 	// reader := csv.NewReader(transform.NewReader(finn, japanese.ShiftJIS.NewDecoder()))
 	// reader := csv.NewReader(transform.NewReader(finn, japanese.EUCJP.NewDecoder()))
 	reader.LazyQuotes = true
+	return reader
+}
 
+func createWriteCSV(fout io.Writer) *csv.Writer {
 	writer := csv.NewWriter(fout) // utf-8
 	// writer := csv.NewWriter(transform.NewReader(fout, japanese.ShiftJIS.NewDecoder()))
 	// writer := csv.NewWriter(transform.NewReader(fout, japanese.EUCJP.NewDecoder()))
 	writer.UseCRLF = true
 	// writer.Comma = '\t'
+	return writer
+}
 
+func src_to_out(reader *csv.Reader, writer *csv.Writer) {
 	log.Printf("Start")
 	for {
 		line, err := reader.Read()
